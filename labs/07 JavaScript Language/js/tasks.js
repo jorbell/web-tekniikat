05953f3075afab9c458663b2adb1f3c1453ea783
jorbell <jori.v.hanninen@gmail.com>
2019-03-29 13:02:00 +0200

    7. tehtävä tehty

--- labs/07 JavaScript Language/js/tasks.js
+++ labs/07 JavaScript Language/js/tasks.js
@@ -13,23 +13,24 @@ function addTask() {
     var task = document.getElementById('task');
 
     // Reference to where the output goes:
-    var output = document.getElementById('output');
+    //var output = document.getElementById('output');
     
     // For the output:
-    var message = '';
+    //var message = '';
 
     if (task.value) {
     
         // Add the item to the array:
         tasks.push(task.value);
+        updatePage();
         
         // Update the page:
-        message = '<h2>To-Do</h2><ol>';
-        for (var i = 0, count = tasks.length; i < count; i++) {
-            message += '<li>' + tasks[i] + '</li>';
-        }
-        message += '</ol>';
-        output.innerHTML = message;
+        //message = '<h2>To-Do</h2><ol>';
+        //for (var i = 0, count = tasks.length; i < count; i++) {
+        //      message += '<li>' + tasks[i] + '</li>';
+        //}
+        //message += '</ol>';
+        //output.innerHTML = message;
         
     } // End of task.value IF.
 
@@ -37,6 +38,40 @@ function addTask() {
     return false;
     
 } // End of addTask() function.
+
+//Function update page
+function updatePage() {
+    var output = document.getElementById('output');
+    var message = '';
+    message = '<h2>To-Do</h2><ol>';
+    for (var i = 0, count = tasks.length; i < count; i++) {
+        message += '<li>' + tasks[i] + '</li>';
+    }
+    message += '</ol>';
+    output.innerHTML = message;
+    
+}
+
+//Function remove dublicates
+function removeDublicates() {
+    var tmpArr = [];
+    var tmp = undefined;
+    for (var i = 0; i < tasks.length; i++) {
+        tmpArr[i] = tasks[i];
+    }
+
+    var len = tasks.length;
+    for (i = 0; i < len; i++) {
+        tmp = tmpArr[0];
+        tmpArr.splice(0, 1);
+
+        if (!(tmpArr.indexOf(tmp) === -1)) {
+            tasks.splice(tasks.indexOf(tmp), 1);
+        }
+    }
+    updatePage();
+    
+}
 
 // Initial setup:
 function init() {
